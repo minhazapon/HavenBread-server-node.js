ec86c5be10f5a719a839129049dc6497dc41c81d
minhazapon <minhajulabedinapon@gmail.com>
2024-09-22 19:07:48 -0700

    added file

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000
 
 
 app.use(cors())
 app.use(express.json());
+
+
+console.log(process.env.DB_USER)
+console.log(process.env.DB_PASS)
 
 
 app.get('/', (req, res) => {
@@ -18,7 +23,7 @@ app.get('/', (req, res) => {
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://aponBase:aponbase50374409@cluster0.ruz4b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ruz4b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
